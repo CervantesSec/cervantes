71b4bea00226fdbcc2c0d8c22d1e0268592af79b
Ruben Mesquida <16049893+mesquidar@users.noreply.github.com>
2024-10-21 11:06:57 +0200

    Update Vuln Templates

--- Cervantes.IFR/VulnTemplates/VulnTemplatesLoadService.cs
+++ Cervantes.IFR/VulnTemplates/VulnTemplatesLoadService.cs
@@ -90,7 +90,8 @@ public class VulnTemplatesLoadService: IVulnTemplatesLoadService
     
     public async Task CreateEnglishGeneral()
     {
-        
+       
+
     }
     
     public async Task CreateEnglishMastg()
@@ -100,6 +101,86 @@ public class VulnTemplatesLoadService: IVulnTemplatesLoadService
     
     public async Task CreateEnglishWstg()
     {
+        Vuln insufficientAuthentication = VulnTemplatesWstgEN.InsufficientAuthentication;
+        insufficientAuthentication.VulnCwes.Add(new VulnCwe { CweId = 287, VulnId = insufficientAuthentication.Id });
+        insufficientAuthentication.VulnCwes.Add(new VulnCwe { CweId = 306, VulnId = insufficientAuthentication.Id });
+        vulnManager.Add(insufficientAuthentication);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln weakPasswordRequirements = VulnTemplatesWstgEN.WeakPasswordRequirements;
+        weakPasswordRequirements.VulnCwes.Add(new VulnCwe { CweId = 521, VulnId = weakPasswordRequirements.Id });
+        vulnManager.Add(weakPasswordRequirements);
+        await vulnManager.Context.SaveChangesAsync();
+
+        Vuln insecureSessionManagement = VulnTemplatesWstgEN.InsecureSessionManagement;
+        insecureSessionManagement.VulnCwes.Add(new VulnCwe { CweId = 384, VulnId = insecureSessionManagement.Id });
+        insecureSessionManagement.VulnCwes.Add(new VulnCwe { CweId = 613, VulnId = insecureSessionManagement.Id });
+        vulnManager.Add(insecureSessionManagement);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln idor = VulnTemplatesWstgEN.Idor;
+        idor.VulnCwes.Add(new VulnCwe { CweId = 639, VulnId = idor.Id });
+        vulnManager.Add(idor);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln crossSiteScripting = VulnTemplatesWstgEN.CrossSiteScripting;
+        crossSiteScripting.VulnCwes.Add(new VulnCwe { CweId = 79, VulnId = crossSiteScripting.Id });
+        vulnManager.Add(crossSiteScripting);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln insecureCryptographicStorage = VulnTemplatesWstgEN.InsecureCryptographicStorage;
+        insecureCryptographicStorage.VulnCwes.Add(new VulnCwe { CweId = 327, VulnId = insecureCryptographicStorage.Id });
+        insecureCryptographicStorage.VulnCwes.Add(new VulnCwe { CweId = 328, VulnId = insecureCryptographicStorage.Id });
+        vulnManager.Add(insecureCryptographicStorage);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln insecureDeserialization = VulnTemplatesWstgEN.InsecureDeserialization;
+        insecureDeserialization.VulnCwes.Add(new VulnCwe { CweId = 502, VulnId = insecureDeserialization.Id });
+        vulnManager.Add(insecureDeserialization);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln sqlInjection = VulnTemplatesWstgEN.SqlInjection;
+        sqlInjection.VulnCwes.Add(new VulnCwe { CweId = 89, VulnId = sqlInjection.Id });
+        vulnManager.Add(sqlInjection);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln brokenAccessControl = VulnTemplatesWstgEN.BrokenAccessControl;
+        brokenAccessControl.VulnCwes.Add(new VulnCwe { CweId = 639, VulnId = brokenAccessControl.Id });
+        brokenAccessControl.VulnCwes.Add(new VulnCwe { CweId = 284, VulnId = brokenAccessControl.Id });
+        brokenAccessControl.VulnCwes.Add(new VulnCwe { CweId = 285, VulnId = brokenAccessControl.Id });
+        vulnManager.Add(brokenAccessControl);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln securityMisconfiguration = VulnTemplatesWstgEN.SecurityMisconfiguration;
+        securityMisconfiguration.VulnCwes.Add(new VulnCwe { CweId = 16, VulnId = securityMisconfiguration.Id });
+        securityMisconfiguration.VulnCwes.Add(new VulnCwe { CweId = 2, VulnId = securityMisconfiguration.Id });
+        vulnManager.Add(securityMisconfiguration);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln sensitiveDataExposure = VulnTemplatesWstgEN.SensitiveDataExposure;
+        sensitiveDataExposure.VulnCwes.Add(new VulnCwe { CweId = 311, VulnId = sensitiveDataExposure.Id });
+        sensitiveDataExposure.VulnCwes.Add(new VulnCwe { CweId = 312, VulnId = sensitiveDataExposure.Id });
+        sensitiveDataExposure.VulnCwes.Add(new VulnCwe { CweId = 319, VulnId = sensitiveDataExposure.Id });
+        vulnManager.Add(sensitiveDataExposure);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln insufficientLoggingMonitoring = VulnTemplatesWstgEN.InsufficientLoggingAndMonitoring;
+        insufficientLoggingMonitoring.VulnCwes.Add(new VulnCwe { CweId = 778, VulnId = insufficientLoggingMonitoring.Id });
+        insufficientLoggingMonitoring.VulnCwes.Add(new VulnCwe { CweId = 223, VulnId = insufficientLoggingMonitoring.Id });
+        vulnManager.Add(insufficientLoggingMonitoring);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln usingComponentsWithKnownVulnerabilities = VulnTemplatesWstgEN.UsingComponentsWithKnownVulnerabilities;
+        usingComponentsWithKnownVulnerabilities.VulnCwes.Add(new VulnCwe { CweId = 1035, VulnId = usingComponentsWithKnownVulnerabilities.Id });
+        vulnManager.Add(usingComponentsWithKnownVulnerabilities);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        Vuln serverSideRequestForgery = VulnTemplatesWstgEN.ServerSideRequestForgery;
+        serverSideRequestForgery.VulnCwes.Add(new VulnCwe { CweId = 918, VulnId = serverSideRequestForgery.Id });
+        vulnManager.Add(serverSideRequestForgery);
+        await vulnManager.Context.SaveChangesAsync();
+        
+        
         
     }
     
